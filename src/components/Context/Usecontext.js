eaf75e2051d6752c83c719c9ea419054e66d9e5b
Neon <jhonrios@hotmail.es>
2021-09-26 11:12:01 -0500

    componentes EnviarRespuesta inconcluso por tiempo1

---
+++ src/components/Context/Usecontext.js
@@ -0,0 +1,26 @@
+import React ,{useState} from 'react'
+/* se creauna constante para estructurar el createContext la cual sera incorporada en el app.js para ser compartido por los demas compoentes, importante exportarla*/
+export const TemaContext = React.createContext()
+
+/* ingresamos los props para ser renderizado en otros componentes */
+function Usecontext(props) {
+
+    /* se crea los estados a ser compartidos  por los componentes */
+    const [contador, setContador]= useState(1)
+    const [puntaje, setPuntaje]= useState(0)
+
+ 
+    
+    return (
+
+        /*vamos a devolver el provaide para todos los componentes hijos  
+        en value especifica como un objeto que se entra a evaluar*/
+       <TemaContext.Provider value= {{contador, setContador,puntaje, setPuntaje}}>
+       
+       {/* al proveedor todo lo que se ponga lo renderiza */}
+          {props.children}
+       </TemaContext.Provider>
+    )
+}
+
+export default Usecontext
